b952c93c5bb4c6e97c815674339548ad9d0934a7
imstewiez <steven.southan97@gmail.com>
2026-05-31 09:34:34 +0100

    Clean legacy liquidation redirect route

--- src/routes/_authenticated/liquidacao.tsx
+++ src/routes/_authenticated/liquidacao.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { Reveal, Stagger } from "@/components/layout/Reveal";
 
 export const Route = createFileRoute("/_authenticated/liquidacao")({
   beforeLoad: () => {
