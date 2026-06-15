ce1a206ab85bd70dc5dfa72a67050fb7282bfb11
imstewiez <steven.southan97@gmail.com>
2026-06-15 15:35:32 +0100

    Redirect legacy admin recipes page to item management

--- src/routes/_authenticated/admin.receitas.tsx
+++ src/routes/_authenticated/admin.receitas.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/admin/receitas")({
   beforeLoad: async () => {
-    throw redirect({ to: "/receitas" });
+    throw redirect({ to: "/admin/itens" });
   },
 });
